e3c7df139872c02defa31f2bd0e21336f504de4e
Bourgeoisie2024 <bwalminity@gmail.com>
2026-07-30 13:37:34 +0200

    Add toast notification support

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Toaster
+      position="top-right"
+      reverseOrder={false}
+      toastOptions={{
+        duration: 3000,
+        style: {
+          borderRadius: '12px',
+          background: '#fff',
+          color: '#111827',
+        },
+      }}
+    />
   </StrictMode>
 );
